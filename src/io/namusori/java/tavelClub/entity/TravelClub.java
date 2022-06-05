b632d6b716672950537633885a8f915b6654d5af
Robb <robb@kakao.com>
2022-06-05 14:16:40 +0900

    Add toString method

--- src/io/namusori/java/tavelClub/entity/TravelClub.java
+++ src/io/namusori/java/tavelClub/entity/TravelClub.java
@@ -6,7 +6,7 @@ import java.util.UUID;
 
 public class TravelClub {
     private static final int MINIMUM_NAME_LENGTH = 3;
-    private static final int MININUM_INTRO_LENGTH = 10;
+    private static final int MINIMUM_INTRO_LENGTH = 10;
     private String id;
     private String clubName;
     private String intro;
@@ -19,19 +19,44 @@ public class TravelClub {
     public TravelClub(String clubName, String intro) {
         this();
         setClubName(clubName);
-        this.intro = intro;
+        setIntro(intro);
         this.foundationDay = DateUtil.today();
     }
 
     public void setClubName(String clubName) {
         if(clubName.length() < MINIMUM_NAME_LENGTH) {
-            System.out.printf("Club name should be longer the 3");
+            System.out.printf("Club name should be longer the " + MINIMUM_NAME_LENGTH);
             return;
         }
         this.clubName = clubName;
     }
 
+    public void setIntro(String intro) {
+        if(intro.length() < MINIMUM_INTRO_LENGTH) {
+            System.out.println("Club Intro should be longer then " + MINIMUM_INTRO_LENGTH);
+            return;
+        }
+        this.intro = intro;
+    }
+
     public String getClubName() {
         return clubName;
+    }
+
+    public String getIntro() { return intro; }
+
+    public String getId() { return id; }
+
+    public String getFoundationDay() { return foundationDay; }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("TravelClub club ID : ").append(id);
+        builder.append(", ClubName : ").append(clubName);
+        builder.append(", Intro").append(intro);
+        builder.append(", FoundationDay : ").append(foundationDay);
+
+        return builder.toString();
     }
 }
